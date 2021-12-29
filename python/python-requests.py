cf2b69d69a9f0d7cca942f05eb6d09b2104799f4
Jannik Malte Meissner <jannik@neuralfinity.com>
2021-12-29 17:41:04 +0100

    fixed python code for requests sample

--- python/python-requests.py
+++ python/python-requests.py
@@ -1,16 +1,10 @@
 import requests
-import json
  
 url = "https://beta.eu-north.computational-magic.com/api/v1"
 
 headers = {'x-api-key': 'INSERT YOUR API KEY HERE!'}
  
 query = """{summary(input: "INSERT YOUR LONG STRING HERE.")}"""
-
-# response = requests.post(url, headers=headers, data=query)
- 
-# print("Status Code", response.status_code)
-# print("Response ", response.text())
 
 response = requests.post(url, headers=headers, json={'query': query})
 print(response.status_code)
